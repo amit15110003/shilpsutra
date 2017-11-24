e0db7c5b6d3644b4394b664a03bf8219b017b6aa
rrptm <raviranjan.ptm@gmail.com>
2017-11-24 22:38:37 +0530

    about
    completed

--- application/views/client/about.php
+++ application/views/client/about.php
@@ -4,28 +4,37 @@
 		<h1>About</h1>
 </div>
 <div class="container">
+	<div class="col-md-offset-5 col-md-6 "><img src="<?php echo base_url();?>/media/image/pic.png" class="img-responsive" style="height: 230px;"></div>
+
 	<div class="col-md-offset-1 col-md-9 col-md-offset-2">
-			<h4>
-				<b>The Hippogriff – Concept</b>
-			</h4>
-			<p style="padding-top: 25px;">Speak Art:</p>
-			<p style="padding-top: 15px;">Everyone needs art. The Hippogriff brings together incredible art work from various genres to you as printed everyday use products. Art is not only accessible to all now, it’s affordable too. To express yourself through art is oddly satisfying. The creative intelligence and visual dexterity expressed through the lush arrangement of colours and inked ideas is celebrated with our quality products.</p>
-			<p style="padding-top: 25px;">The Community:</p>
-			<p style="padding-top: 15px;">We are building a strong community of talented artists from around the world. We collaborate with distinguished individuals who create unique hand drawn, oil painted, illustrated, photographed works of art that are then mocked up and displayed as their own products. The best part? Every sale helps pay an artist!</p>
-			<p style="padding-top: 25px;">The Canvas:</p>
-			<p style="padding-top: 15px;">The vision is to make individuals identify themselves with art. We want to print, paint and cover as many articles or products possible with exceptional artwork created by more and more artists. The Hippogriff is home for all future artists and their creations.</p>
-			<h4 style="padding-top: 25px;">
-				<b>The Hippogriff – Company</b>
-			</h4>
-			<p style="padding-top: 25px;">About :</p>
-			<p style="padding-top: 15px;">The Hippogriff is a mythological creature that has the front portion of an eagle and hind portion of a horse. It is a majestic, powerful, fast, and inspiring – Everything that is synonymous to the brand’s identity and that is why it has been chosen as a symbol of the brand and all that it represents.</p>
-			<p style="padding-top: 15px;">The company primarily deals in printed products with licensed artwork as the design used. Based in India, the company uses top notch facilities and equipment to get a manufacturing edge as well as bringing a sense of authenticity to the final customer when they receive the product. The company firmly believes in superior service, state of the art craftsmanship and bringing something new to the table with new age designs.</p>
-			<p style="padding-top: 25px;">Story:</p>
-			<p style="padding-top: 15px;">“ Early 2015, I had the urge to start something of my own. Being and avid online shopper myself  I had a certain attraction the market present then, speaking with the context of India. The idea at this time was rudimental and borderline amateur. I had to hop from one website to another to find things I really liked, and even though that itself was inconvenient I was gradually made aware of a bigger gap. The content available at all these places were extremely basic, childish, repetitive and lacked intellect. I wondered why content and designs these days are dumbed down for the people and why they were produced and sold without any thought put into any of them.</p>
-			<p style="padding-top: 15px;">At the same time I was filled with awe with the kind of artwork I was witnessing on social media by independent artists and illustrators. I knew right there that combining these two segments would fill the gap I was dying to come out of. The idea was to take a painting of a wall, a gallery exhibition, an illustrator artboard or a standard sketchbook and put it on everyday products. If a person wanted to wear a T-shirt, they should have an option of sporting an artwork made with hours of dedication and priceless grey matter that a talented artist somewhere across the globe has conjured skilfully. I wanted to help budding artists get as many eyes and pay them for their contribution to my venture. At the same time I wanted to offer a choice to the people to consume better content. Quoting Steve Jobs ,’Be a yardstick of quality. Some people aren’t used to an environment where excellence is expected’. I’m confident about where we are heading and even more thrilled about giving art lovers what they want. “</p>
-			<p style="padding-top: 25px;">-Allen George</p>
-			<p style="padding-top: 25px;">Founder</p>
-			<p style="padding-top: 25px;">The Hippogriff</p>
+			<h3>
+				<b>Handcrafted, Bridal, Casual, Dressy, Unique & Designer Juttis from India</b>
+			</h3>
+
+				<p style="padding-top: 15px;">Exclusively created by SHILPA AGARWALLA, an alumnus from NIFT, Delhi, SHILPSUTRA conveys to you a line of bespoke fine leather juttis, that talk the appeal of a genuine old fashioned craftsman with a craving to reproduce high road form for those that wear, appreciate style, class and culture.<br><br>
+
+				Brilliant hues, clean bends, faultless complete, duty to quality and uniqueness of item; we at SHILPSUTRA take incredible pride in all that we put out on our racks.<br><br>
+
+				Shilpa Agarwalla set up and propelled SHILPSUTRA in 2015, with a dream to bring some of India's best handiworks on the steadily developing worldwide design grounds.<br><br>
+
+				Our juttis are made in fine leather and are easily twisted in hand, making it extremely soft on your feet. All our products are hand embroidered too. Another uniqueness of our juttis are, they are all "chottapanjas" ? (panja is the zone that covers the fingers of your feet). The covering range is around 2 and a half to three inches. In more established circumstances, when ladies remained in cloak, they could just uncover their feet. The shoe producer of yester years, made chottapanja juttis for regal women, as it made their feet look dainty and lovely.</p>
+				
+					
+			
+			<h3 style="padding-top: 25px;"> <b>OUR JUTTIS ARE PERFECT FOR GIFTS </b></h3>
+			<p style="padding-top: 15px;">A present for yourself, or somebody unique, SHILPSUTRA offers an excellent scope of extraordinary carefully assembled pieces, that make culminate presents for your loved ones. All our juttis come in hard cover boxes, with juttis wrapped in spread paper. The crate contains additional embellishments, a care card as well.</p>
+
+			<h3 style="padding-top: 25px;"> <b>CARE FOR YOUR TREASURE </b></h3>
+			<p style="padding-top: 15px;">Our Juttis are made of genuine leather and can be easily twisted for comfort. Our design is called Chotta Punja, which was worn by some of the most beautiful queens in the world.<br><br>
+
+			We recommend that you always keep the juttis wrapped in muslin cloth. Muslin cloth lets the product breathe easily and keeps it shining for long.<br><br>
+
+			You would generally not feel the “first pinch” when you wear our Juttis but just in case you do, please apply a very small quantity of mustard oil on the internal line using a cloth soaked in mustard oil. This will soften the leather seam within 24 hours and make it comfortable to wear.<br><br>
+
+			SHILPSUTRA to our customers means to be an equivalent word for striking plans that reflect Punjabi extravagance, uniqueness, and emanation with as enchanting blend of present day chick look, additionally exemplary in a crisp and energizing way. Indeed, no plans are indistinguishable at SHILPSUTRA.</p>
+
+			<h3 style="padding-top: 25px;"> <b>Be Unique You – SHILPSUTRA</b></h3>
+			
 	</div>
 		
 </div>
