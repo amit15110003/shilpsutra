c1eed895fff74f70ed011038ea8848fecf2eb0a8
amit15110003 <amitanand.kvj.2@gmail.com>
2017-11-25 10:24:36 +0530

    ads

--- application/views/client/index.php
+++ application/views/client/index.php
@@ -1,6 +1,6 @@
 	<div class="spacer"></div>
 	<div id="startchange"></div>
-        <div id="carousel-example-generic " class="carousel slide" data-ride="carousel" style="height:450px;margin-top: 125px;">
+        <div id="carousel-example-generic " class="carousel slide" data-ride="carousel" style="margin-top: 125px;">
     	  <!-- Indicators -->
     	  <ol class="carousel-indicators">
     	    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
@@ -12,45 +12,47 @@
     	  <!-- Wrapper for slides -->
     	  <div class="carousel-inner" role="listbox">
     	    <div class="item active">
-    	      <img class="fill" src="<?php echo base_url();?>/media/image/Home-banner_2-1720x890.jpg" alt="..." >
+    	      <img class="img-responsive" src="https://www.shilpsutra.com/upload/stowlcarousel/launching-gulabo.png" alt="..." >
     	    </div>
     	    <div class="item">
-    	      <img class="fill" src="<?php echo base_url();?>/media/image/Home-banner-1720x890.jpg" alt="..." >
+    	      <img class="img-responsive" src="https://www.shilpsutra.com/upload/stowlcarousel/launching-gulabo.png" alt="..." >
     	    </div>
     	    <div class="item">
-    	      <img class="fill" src="<?php echo base_url();?>/media/image/LAPTOPSLEEVES_fINAL_-1.jpg" alt="...">
+    	      <img class="img-responsive" src="https://www.shilpsutra.com/upload/stowlcarousel/launching-gulabo.png" alt="...">
     	    </div>
     	    <div class="item">
-    	      <img class="fill" src="<?php echo base_url();?>/media/image/slider3-hp2_.jpg" alt="...">
+    	      <img class="img-responsive" src="https://www.shilpsutra.com/upload/stowlcarousel/launching-gulabo.png" alt="...">
     	    </div>
     	</div>
 
     <div class="gap"></div>
 	<div class="container">
-		<div class="row">
+		<div class="row product-banner-row">
 			<div class="col-md-4 ">
-				<div class="product-banner banner-small" >
-					<img src="https://www.shilpsutra.com/1296-home_default/mint-magic.jpg" class="img-responsive center-block">
+				<div class="product-banner">
+					<img src="https://www.shilpsutra.com/1296-home_default/mint-magic.jpg" class="img-responsive center-block  banner-small" >
 				</div>
 			</div>
 			<div class="col-md-4 ">
-				<div class="product-banner banner-small">
-					<img src="https://www.shilpsutra.com/1296-home_default/mint-magic.jpg" class="img-responsive center-block">
+				<div class="product-banner ">
+					<img src="https://www.shilpsutra.com/1296-home_default/mint-magic.jpg" class="img-responsive center-block  banner-small">
 				</div>
 			</div>
 			<div class="col-md-4 ">
-				<div class="product-banner banner-small">
-					<img src="https://www.shilpsutra.com/1296-home_default/mint-magic.jpg" class="img-responsive center-block">
+				<div class="product-banner ">
+					<img src="https://www.shilpsutra.com/1296-home_default/mint-magic.jpg" class="img-responsive center-block  banner-small">
 				</div>
 			</div>
+		</div>
+		<div class="row product-banner-row">
 			<div class="col-md-8 ">
-				<div class="product-banner banner-large">
-					<img src="https://steemitimages.com/0x0/https://steemitimages.com/DQmcnzhsv7J3PeH5HHqe9NfGQuRmH4BYRdEiDw7BnoC15us/Untitled-3.png" class="img-responsive center-block">
+				<div class="product-banner ">
+					<img src="https://www.shilpsutra.com/upload/stowlcarousel/designer-juttis.png" class="img-responsive center-block banner-large">
 				</div>
 			</div>
 			<div class="col-md-4">
-				<div class="product-banner banner-large">
-					<img src="http://www.indigenousaction.org/wp-content/uploads/banner-making-infographic-585x1024-1-585x1024.jpg" class="img-responsive center-block">
+				<div class="product-banner">
+					<img src="http://www.indigenousaction.org/wp-content/uploads/banner-making-infographic-585x1024-1-585x1024.jpg" class="img-responsive center-block banner-large">
 				</div>
 			</div>
 		</div>
@@ -58,7 +60,8 @@
     <div class="gap"></div>
     <div class="row" style="height: 600px;">
     	<div class="col-md-6" style="padding:5%;background-color: #98ded6;height: 100%;">
-    		<div style="padding-top: 5%;padding-bottom:5%;background-color: #fcf6c6;border-radius: 20px;height: 100%;">
+    		<div style="padding-top: 5%;padding-bottom:5%;background-color: #fcf6c6;border-radius: 20px;height: 100%;
+ box-shadow: 0 2px 3px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);">
     			<div style="background-color: #fff;height: 4px;"></div>
     			<div class="col-xs-12 col-sm-12 col-md-12" style="margin:auto;padding: 5%;">
 			      <div class="carousel carousel-showmanymoveone slide" id="itemslider">
@@ -105,5 +108,22 @@
     	</div>
     	<div class="col-md-6" style="background-color: #ffbdbe;height: 600px;">
     		
+    	</div>
+    </div>
+    <div class="container-fluid">
+    	<div class="col-md-3">
+    		<div class="product-circle">
+    			<img src="">
+    		</div>
+    	</div>
+    </div>
+    <div class="container-fluid">
+    	<div class="col-md-12 " style="padding:10%;">
+    		<img src="https://media.chumbak.com/media/prismic/ad8bb3399c24a980f314510ef7600cc6c0df32e9_fanreel_d.jpg" class="img-responsive center-block">
+    	</div>
+    	<div class="col-md-offset-4 col-md-4 text-center" style="background-color: #fff; opacity: 0.98;position: absolute;display: block;padding: 2% 1% 2% 1%;margin-top: 15%;">
+    		<h3>#shilpsutra</h3>
+    		<h4>Tag your pictures on instagram with #shilpsutra and get featured in fanreel.</h4>
+    		<button style="background-color:#fff;border:solid 1px #000;padding: 16px 32px;">SHOP LOOKS</button>
     	</div>
     </div>
